09ec7489c1a195c4cc06c868986e9fcbcd5fd9b8
PhilippC <crocoapps@googlemail.com>
2013-09-22 06:53:18 +0200

    First version of test to bind android-filechooser, not yet successful

---
+++ MonoFileChooserApp/Activity1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MonoFileChooserApp
+{
+	[Activity(Label = "MonoFileChooserApp", MainLauncher = true, Icon = "@drawable/icon")]
+	public class Activity1 : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+			button.Click += delegate
+				{
+					Intent i = Keepass2android.Kp2afilechooser.Kp2aFileChooserBridge.GetLaunchFileChooserIntent(this);
+					StartActivityForResult(i, 0);
+				};
+		}
+	}
+}
+
